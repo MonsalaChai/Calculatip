09dd40cfb5d0885d8d8ac39c551c3e437a334c41
Mesalu <samuel@mesalu.com>
2019-05-04 23:42:12 -0700

    Rearrange some stuff to be a dedicated view.

---
+++ app/src/main/java/com/monsalachai/calculatip/ui/InvestedPartyView.java
@@ -0,0 +1,27 @@
+package com.monsalachai.calculatip.ui;
+
+import android.content.Context;
+import android.support.constraint.ConstraintLayout;
+import android.util.AttributeSet;
+
+public class InvestedPartyView extends ConstraintLayout {
+    
+    public InvestedPartyView(Context context) {
+        super(context);
+        init();
+    }
+
+    public InvestedPartyView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public InvestedPartyView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    private void init() {
+
+    }
+}
