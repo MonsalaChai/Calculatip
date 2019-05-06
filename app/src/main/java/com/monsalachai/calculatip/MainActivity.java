a76aa4dac84fe21fcc7b3916d3f6f2b7975a3b92
Christina Kaetz <goddess2english@gmail.com>
2019-05-05 21:31:55 -0700

    Added items to the options menu and made an empty dialog that pops up if you select 'settings'

--- app/src/main/java/com/monsalachai/calculatip/MainActivity.java
+++ app/src/main/java/com/monsalachai/calculatip/MainActivity.java
@@ -1,10 +1,13 @@
 package com.monsalachai.calculatip;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -48,10 +51,34 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        switch (id)
+        {
+            case R.id.action_settings_menu:
+                // Open settings menu, where people can:
+                // 1. Set app color
+                // 2. Set default % to start at
+                // 3. Other ideas?
+                showSettingsMenu();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
 
-        return super.onOptionsItemSelected(item);
+        }
+    }
+    void showSettingsMenu()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final View DialogView = getLayoutInflater().inflate(R.layout.dialog_menu_settings, null);
+        builder.setView(DialogView).setTitle("Settings Menu")
+                .setPositiveButton("Submit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // save any settings set
+                        Log.d("Settings Menu", "Need to save settings");
+                    }
+                })
+                .setNegativeButton("Cancel", null);
+        builder.create().show();
+
     }
 }
