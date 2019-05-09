9764943e2bfce736a3fee053d36fce624fec42e9
Mesalu <samuel@mesalu.com>
2019-05-08 18:45:10 -0700

    Create basic adapter

---
+++ app/src/main/java/com/monsalachai/calculatip/ui/ParticipantAdapter.java
@@ -0,0 +1,37 @@
+package com.monsalachai.calculatip.ui;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.monsalachai.calculatip.R;
+
+public class ParticipantAdapter extends RecyclerView.Adapter<ParticipantAdapter.ViewHolder> {
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ViewHolder(View view) {
+            super(view);
+        }
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_invested_party, parent, false);
+
+        ViewHolder vh = new ViewHolder(view);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
